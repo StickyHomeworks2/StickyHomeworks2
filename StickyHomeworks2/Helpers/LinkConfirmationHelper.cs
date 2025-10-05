bc66240c333d311a4ae484778984fb808814edb6
zji573432-cpu <zji573432@gmail.com>
2025-10-06 00:31:21 +0800

    feat:新增对用户打开外部链接的提醒

---
+++ StickyHomeworks2/Helpers/LinkConfirmationHelper.cs
@@ -0,0 +1,39 @@
+﻿using StickyHomeworks2.Views;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StickyHomeworks2.Helpers
+{
+    public static class LinkConfirmationHelper
+    {
+        public static void ConfirmAndOpenLink(string linkUri, string linkText)
+        {
+            // 弹出确认窗口
+            ConfirmLinkWindow confirmWindow = new ConfirmLinkWindow(linkUri, linkText);
+            confirmWindow.ShowDialog();
+
+            // 如果用户确认打开链接
+            if (confirmWindow.IsConfirmed)
+            {
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = linkUri,
+                    UseShellExecute = true
+                };
+                try
+                {
+                    Process.Start(psi);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"无法打开链接: {ex.Message}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
---
+++ StickyHomeworks2/Views/ConfirmLinkWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StickyHomeworks2.Views
+{
+    /// <summary>
+    /// ConfirmLinkWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class ConfirmLinkWindow : Window
+    {
+        public bool IsConfirmed { get; private set; }
+
+        public ConfirmLinkWindow(string linkUri, string linkText)
+        {
+            InitializeComponent();
+            LinkUriTextBlock.Text = linkUri;
+            LinkTextTextBlock.Text = linkText;
+            IsConfirmed = false;
+        }
+
+        private void YesButton_Click(object sender, RoutedEventArgs e)
+        {
+            IsConfirmed = true;
+            this.Close();
+        }
+
+        private void NoButton_Click(object sender, RoutedEventArgs e)
+        {
+            IsConfirmed = false;
+            this.Close();
+        }
+    }
+}
